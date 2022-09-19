655a07e23706812d30d8956bb3a9dc8e73a5e1df
Shahriar Parvez <mr.spshuvo@gmail.com>
2022-09-19 23:05:04 +0600

    :zap: fixed formatting

--- data/help-contents.js
+++ data/help-contents.js
@@ -2,173 +2,225 @@ export const data = {
   'integration-issues': {
     empty_deviceId: {
       platforms: ['all-platforms', 'web'],
-      products: ['all-products', 'video-calling', 'voice-calling'],
+      products: ['all-products', 'video-calling'],
       popular: false,
     },
-
     video_profile: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     web_camera_light: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     android_startaudiomixing_permission: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     ncs_vs_query: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     unity_crash: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     local_network_privacy_rtm: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     web_recording: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     qps_client: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     agora_class_custom_properties: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     local_network_privacy: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     channel: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     recording_video_profile: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
+    token_cohost: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+    system_volume: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+    switch_screen_camera_web: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+    string: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+    recording_player: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+    recording_mode: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+    reconnection: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+    log_native: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+    live_streaming_disconnection_web: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+    host_set_role: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+    custom_switch_default: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+    console_error_web: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+    cdnstreaming_callback_relationship: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+    call_duration: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+    audience_event: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
     abnormal_exit: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     restful_api_call_frequency: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     streaminit_error: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     rtm_compilation_error: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     judge_voice_video_call: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     no_stereo_audio: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     recording_faq: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     framework_cannot_be_opened: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     video_enhancement: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     image_enhancement: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     profile_difference: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     cmd_control_record: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     fail_to_upload: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     acquire_file_directory: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     reduce_app_size_rtc: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     stop_cloud_recording: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+    _log_web: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
@@ -180,115 +232,101 @@ export const data = {
       products: ['all-products'],
       popular: false,
     },
-
     video_bighead: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     video_freeze: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     audio_noise: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     unsynchronized_video: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     video_blank: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     ios_bluetooth: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     echo: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     audio_freeze: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     'web-native_video_issues': {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     device_occupied: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     video_camera: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
+    record_status_error: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
     record_split: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     pixelated_green_video: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     record_file_issue: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     audio_noaudio: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     audio_role: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     video_blur: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     track_ended: {
       platforms: ['all-platforms'],
       products: ['all-products'],
       popular: false,
     },
-
     audio_low: {
       platforms: ['all-platforms'],
       products: ['all-products'],
