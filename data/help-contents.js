dcf7be14774c2c9c1b01036811590f712f4ddad0
Shahriar Parvez <mr.spshuvo@gmail.com>
2022-09-19 08:52:45 +0600

    :zap: add data dirs

---
+++ data/help-contents.js
@@ -0,0 +1,445 @@
+export const data = {
+  'integration-issues': {
+    empty_deviceId: {
+      platforms: ['all-platforms', 'web'],
+      products: ['all-products', 'video-calling', 'voice-calling'],
+      popular: false,
+    },
+
+    video_profile: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    web_camera_light: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    android_startaudiomixing_permission: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    ncs_vs_query: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    unity_crash: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    local_network_privacy_rtm: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    web_recording: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    qps_client: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    agora_class_custom_properties: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    local_network_privacy: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    channel: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    recording_video_profile: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    abnormal_exit: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    restful_api_call_frequency: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    streaminit_error: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    rtm_compilation_error: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    judge_voice_video_call: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    no_stereo_audio: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    recording_faq: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    framework_cannot_be_opened: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    video_enhancement: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    image_enhancement: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    profile_difference: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    cmd_control_record: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    fail_to_upload: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    acquire_file_directory: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    reduce_app_size_rtc: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    stop_cloud_recording: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+  },
+  'quality-issues': {
+    web_screen_share_issues: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    video_bighead: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    video_freeze: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    audio_noise: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    unsynchronized_video: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    video_blank: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    ios_bluetooth: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    echo: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    audio_freeze: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    'web-native_video_issues': {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    device_occupied: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    video_camera: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    record_split: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    pixelated_green_video: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    record_file_issue: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    audio_noaudio: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    audio_role: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    video_blur: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    track_ended: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    audio_low: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+  },
+  'general-product-inquiry': {
+    onpremise_cloud: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    platform_version: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    recording_concurrence: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    rtm_emoji: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    agora_product: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    rtm_concurrency: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    capacity: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    audio_format: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    conversion_limitation: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    call_api_in_browser: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    vr_headset: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    agora_class_state: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    differ_agora_cdn: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    browser_support: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+  },
+  'account-and-billing': {
+    billing_free: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    email_verification: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    billing_basis: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    rtm_project_suspended: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    console_account_faq: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    billing_account: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+  },
+  'other-issues': {
+    sei: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    android_background: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    android_noaudio: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+
+    api_download: {
+      platforms: ['all-platforms'],
+      products: ['all-products'],
+      popular: false,
+    },
+  },
+};
---
+++ data/help-faqs.js
@@ -0,0 +1,27 @@
+export const data = [
+  {
+    question: 'Free Minutes and billing details',
+    link: '/help/integration-issues/empty_deviceId',
+  },
+  {
+    question: 'I cannot hear any voice in a call',
+    link: '/help/integration-issues/video_profile',
+  },
+  {
+    question: 'Black screen occurs in a call',
+    link: '/help/integration-issues/unity_crash',
+  },
+  {
+    question:
+      'I connect a Bluetooth to a phone, but not all calls are answered through the Bluetooth headset',
+    link: '/help/integration-issues/web_camera_light',
+  },
+  {
+    question: 'How to pass the basic HTTP authentication',
+    link: '/help/integration-issues/ncs_vs_query',
+  },
+  {
+    question: 'Does Agora have reconnection mechanisms?',
+    link: '/help/integration-issues/web_recording',
+  },
+];
